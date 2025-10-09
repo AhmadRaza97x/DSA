7b45a3b32e086e6d30954e31cf3bf98ea65cd84a
AhmadRaza97x <ahmadraza97x@gmail.com>
2025-10-09 12:19:25 +0530

    initial commit

---
+++ 2_String.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>                 // ADD ALL CPP LIBRARIES
+using namespace std;
+int main ()
+{
+    string s;
+    cout << "Enter String : ";
+    // cin >> s;                        // IT TAKE A WORD ONLY
+    // cout << s;   
+
+    getline(cin,s);                     // IT TAKE A LINE AS A INPUT
+    cout << s;
+    return 0;
+}
