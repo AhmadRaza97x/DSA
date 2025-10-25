e45fe1f56be509c9c15880fe1e3e58ffa328a118
mohammad2k3 <ahmadraza97x@gmail.com>
2025-10-25 13:35:10 +0530

    delete files

--- c.c
+++
@@ -1,6 +0,0 @@
-#include<stdio.h>
-int main(int argc, char const *argv[])
-{
-    printf("HELLO mohammad AHMAD RAZA");
-    return 0;
-}
--- d.c
+++
@@ -1,6 +0,0 @@
-#include<stdio.h>
-int main(int argc, char const *argv[])
-{
-    printf("MOHAMMAD SIRAJUDDIN");
-    return 0;
-}
